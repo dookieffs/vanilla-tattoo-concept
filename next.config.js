cb22b325bcccaad4b6b71f2cfbf0128384e14134
dusan <dusanqt@gmail.com>
2024-10-18 17:50:13 +0200

    output export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withNextIntl = require("next-intl/plugin")();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
